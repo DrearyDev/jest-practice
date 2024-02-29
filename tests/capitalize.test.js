1f68a82faecffc7f52960d228336e08aef1799a7
drearydev <143649090+DrearyDev@users.noreply.github.com>
2024-02-29 17:56:03 -0500

    Add more tests

--- tests/capitalize.test.js
+++ tests/capitalize.test.js
@@ -2,4 +2,6 @@ import { capitalize } from "../scripts/capitalize.js";
 
 test('Return string with first letter Capitalized', () => {
     expect(capitalize('string')).toBe('String');
+    expect(capitalize('mixedCapitals')).toBe('MixedCapitals');
+    expect(capitalize('ALLCAPS')).toBe('ALLCAPS');
 });
